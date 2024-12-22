afbb4292251d9e8a431da4d510eebd4e9a98c72c
Mitch Patenaude <mitch@mitchpatenaude.net>
2024-12-21 16:43:25 -0800

    First test of loading files

--- main.go
+++ main.go
@@ -3,18 +3,49 @@ package main
 import (
   "fmt"
   "github.com/pneumaticdeath/golife"
+  "os"
+  "strings"
 )
 
-func main() {
+func display(g *golife.Game) {
+    min_cell, max_cell := g.BoundingBox()
+    width := max_cell.X - min_cell.X + 1
 
-   pop := make(golife.Population)
-   cells := []golife.Cell{{0, 0}, {0, 1}, {0, 2}}
-
-   pop.Add(cells)
-
-   for i := 0; i < 10; i++ {
-      fmt.Println(pop)
-      pop = pop.Step()
+    fmt.Printf("Generation %d\n", g.Generation)
+    if max_cell.X >= min_cell.X {
+        fmt.Println("Bounding Box",min_cell,"to",max_cell)
+        var x, y int64
+        fmt.Printf("+%s+\n", strings.Repeat("-", int(width * 2 - 1)))
+        for y = min_cell.Y; y <= max_cell.Y; y++ {
+            cell_line := make([]string, width)
+            for x = min_cell.X; x <= max_cell.X; x++ {
+                if g.Population[golife.Cell{x, y}] {
+                    cell_line[int(x - min_cell.X)] = "*"
+                } else {
+                    cell_line[int(x - min_cell.X)] = " "
+                }
+            }
+            fmt.Printf("|%s|\n", strings.Join(cell_line, " "))
+        }
+        fmt.Printf("+%s+\n", strings.Repeat("-", int(width * 2 - 1)))
     }
-    fmt.Println(pop)
+}
+
+func main() {
+    var g *golife.Game
+
+    if len(os.Args) > 1 {
+        g = golife.Load(os.Args[1])
+    } else {
+        cells := []golife.Cell{{0, 0}, {0, 1}, {0, 2}, {1, 0}, {2, 1}}
+        g = &golife.Game{}
+        g.Init()
+        g.Population.Add(cells)
+    }
+
+    for i := 0; i < 100 && len(g.Population) > 0; i++ {
+        display(g)
+        g.Next()
+    }
+    display(g)
 }
